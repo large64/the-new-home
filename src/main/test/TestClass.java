8a350c91f26d9f40ac4ddfb461b7190f73563dc1
large64 <zadoridenes@gmail.com>
2015-09-06 15:22:53 +0200

    Initial commit

---
+++ src/main/test/TestClass.java
@@ -0,0 +1,10 @@
+package main.test;
+
+/**
+ * Created by large64 on 9/6/15.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}

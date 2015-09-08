dd297d368af1d1c2dbdab3b0de56d8883fa22ed9
large64 <zadoridenes@gmail.com>
2015-09-08 22:29:27 +0200

    Add shaders (not working yet)

---
+++ src/shaders/StaticShader.java
@@ -0,0 +1,19 @@
+package shaders;
+
+/**
+ * Created by large64 on 2015.09.08..
+ */
+public class StaticShader extends ShaderProgram {
+
+    private static final String VERTEX_FILE = "src/shaders/vertexShader";
+    private static final String FRAGMENT_FILE = "src/shaders/fragmentShader";
+
+    public StaticShader() {
+        super(VERTEX_FILE, FRAGMENT_FILE);
+    }
+
+    @Override
+    protected void bindAttributes() {
+        super.bindAttribute(0, "position");
+    }
+}

3c5dff7296566d5dfcbf8e24ce618a28b73f242a
large64 <zadoridenes@gmail.com>
2015-09-08 19:57:51 +0200

    Render quad (test)

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,6 +2,9 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 /**
  * Created by large64 on 9/6/15.
@@ -10,10 +13,27 @@ public class MainGameLoop {
     public static void main(String[] args) {
         DisplayManager.createDisplay();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f,
+        };
+
+        RawModel model = loader.loadToVAO(vertices);
+
         while(!Display.isCloseRequested()) {
+            renderer.prepare();
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
 
+        loader.cleanUp();
         DisplayManager.closeDisplay();
     }
 }

488e908a0e095ef1fe4c142c4e6af5ab5a6286ba
large64 <zadoridenes@gmail.com>
2015-09-14 19:22:44 +0200

    7th video done

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,5 +1,7 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 /**
  * Created by large64 on 2015.09.08..
  */
@@ -7,14 +9,24 @@ public class StaticShader extends ShaderProgram {
 
     private static final String VERTEX_FILE = "src/shaders/vertexShader";
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader";
+    private int location_transformationMatrix;
 
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
     }
 
     @Override
+    protected void getAllUniformLocations() {
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+    }
+
+    @Override
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
         super.bindAttribute(1, "textureCoords");
+    }
+
+    public void loadTransformationMatrix(Matrix4f matrix) {
+        super.loadMatrix(location_transformationMatrix, matrix);
     }
 }

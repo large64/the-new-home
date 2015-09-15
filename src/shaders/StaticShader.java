809d8d1e3cc04519466046e68f6a225a3a6b7c84
large64 <zadoridenes@gmail.com>
2015-09-15 15:40:40 +0200

    Video 8 done

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,6 +1,8 @@
 package shaders;
 
+import entities.Camera;
 import org.lwjgl.util.vector.Matrix4f;
+import toolbox.Maths;
 
 /**
  * Created by large64 on 2015.09.08..
@@ -9,7 +11,10 @@ public class StaticShader extends ShaderProgram {
 
     private static final String VERTEX_FILE = "src/shaders/vertexShader";
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader";
+
     private int location_transformationMatrix;
+    private int location_projectionMatrix;
+    private int location_viewMatrix;
 
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -18,6 +23,8 @@ public class StaticShader extends ShaderProgram {
     @Override
     protected void getAllUniformLocations() {
         location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+        location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+        location_viewMatrix = super.getUniformLocation("viewMatrix");
     }
 
     @Override
@@ -28,5 +35,14 @@ public class StaticShader extends ShaderProgram {
 
     public void loadTransformationMatrix(Matrix4f matrix) {
         super.loadMatrix(location_transformationMatrix, matrix);
+    }
+
+    public void loadViewMatrix(Camera camera) {
+        Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+        super.loadMatrix(location_viewMatrix, viewMatrix);
+    }
+
+    public void loadProjectionMatrix(Matrix4f matrix) {
+        super.loadMatrix(location_projectionMatrix, matrix);
     }
 }

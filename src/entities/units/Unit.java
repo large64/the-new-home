75e3f16777693e5bc3ad292947717f2b580dad1c
large64 <zadoridenes@gmail.com>
2015-10-10 21:41:36 +0200

    Create default constructor for Unit

--- src/entities/units/Unit.java
+++ src/entities/units/Unit.java
@@ -8,6 +8,8 @@ import org.lwjgl.util.vector.Vector3f;
  * Created by large64 on 10/10/15.
  */
 public class Unit extends Entity {
+    public Unit() {}
+
     public Unit(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
         this.model = model;
         this.position = position;
